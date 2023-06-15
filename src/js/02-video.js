9d904b2d6b05641e7e1aba27cd45dae1e86c1814
KatanES <businesskatan.es@gmail.com>
2023-06-15 11:59:03 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+  saveCurrentTime(data.seconds);
+});
+console.log(player);
+
+window.addEventListener('beforeunload', function () {
+  player.getCurrentTime().then(function (time) {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+});
+
+window.addEventListener('load', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+});
